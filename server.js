3dd8ffa053da2a8b269a1173a518d641fcd78a09
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-09-23 18:18:25 +0200

    Update server.js
    created the server.js page according to the tasks from Module 2.1
    
    Including the http module
    including the url module
    includnig the fs module

--- server.js
+++ server.js
@@ -17,19 +17,19 @@ const http = require('http'),
 http.createServer((request, response) => {
 
   // declared addr variable, which has been assigned the function request.url which is the first argument from the create server function
-  let addr = 'http://localhost:8080/documentation.html',
+  let addr = request.url,
 
   // url parse function is used on the addr variable and the results are set to new variable q
   q = url.parse(addr, true),
 
-  // filepath
-  filePath = 'C\Users\Florian\Desktop\CF\movie_api';
+  // filepath will be determined later in the if statement
+  filePath = '';
 
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
     if (err) {
       console.log(err);
     } else {
-      console.log('Added to log.'');
+      console.log('Added to log.');
     }
   });
 
@@ -39,12 +39,15 @@ http.createServer((request, response) => {
     filePath = 'index.html';
   }
 
-  fs.readFile(filePath, (err, data)) => {
+  fs.readFile(filePath, (err, data) => {
     if (err) {
       throw err;
     }
-  }
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  });
 
 // lstens to a responseon port 8080
 }).listen(8080);
-console.log('My first Node test server is running on Port 8080.');
+console.log('My test server is running on Port 8080.');
