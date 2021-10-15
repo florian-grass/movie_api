34fe33761121332b4acab636a2034532acbcf41c
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-15 10:38:24 +0200

    Update passport.js
    trying to find mistakes for the 2.9 task.
    changing small stuff here and there

--- passport.js
+++ passport.js
@@ -5,28 +5,26 @@ const passport = require('passport'),
     passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
 
+// LocalStrategy takes a username and password from the request body
+// and Mongoose to check your database for a user with the same username
 passport.use(new LocalStrategy({
     usernameField: 'Username',
     passwordField: 'Password'
 }, (username, password, callback) => {
-    console.log(username + ' ' + password);
+    console.log(username + '  ' + password);
     Users.findOne({ Username: username }, (error, user) => {
         if (error) {
             console.log(error);
             return callback(error);
         }
-
+        // callback for incorrect user
         if (!user) {
             console.log('incorrect username');
-            return callback(null, false, {message: 'Incorrect username.'});
-        }
-
-        if (!user.validatePassword(password)) {
-            console.log('incorrect password');
-            return callback(null, false, {message: 'Incorrect password.'});
+            // response when the username or password can not be found or incorrect
+            return callback(null, false, {message: 'Incorrect username or password.'});
         }
 
         console.log('finished');
@@ -34,14 +32,14 @@ passport.use(new LocalStrategy({
     });
 }));
 
-
+// JWTStrategy allows you to authenticate users based on the JWT submitted alongside their request
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'    
+    secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
     return Users.findById(jwtPayload._id)
         .then((user) => {
-            return callback(null, user);            
+            return callback(null, user);
         })
         .catch((error) => {
             return callback(error)
