5ab5676dd744f3e23e5b9937f376b59a775a4adf
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-15 10:36:46 +0200

    Update auth.js
    beautifying auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the 
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
-require('./passport'); // Your local passport file
+require('./passport'); // local passport file
 
 
 let generateJWTToken = (user) => {
