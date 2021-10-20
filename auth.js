a81130effa10b4438f062782655acded60570e81
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-20 06:51:52 +0200

    Update auth.js
    beautifying the code

--- auth.js
+++ auth.js
@@ -1,15 +1,15 @@
-// This file contains the actual logic that will authenticate registered users when they log in
+// This code will authenticate login requests using basic HTTP authentication and generates a JWT for the user
 
 const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
 
-require('./passport'); // local passport file
+require('./passport'); // Your local passport file
 
 
 let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {      
+  return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
     expiresIn: '7d', // This specifies that the token will expire in 7 days
     algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
