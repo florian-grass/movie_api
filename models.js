fce9821588bdfc276b4b6c588af350e91a1be805
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-20 06:54:56 +0200

    Update models.js
    Updated modules.js for elements needed for task 2.10
    uncommenting them as soon as task 2.9 is working

--- models.js
+++ models.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// bcrypt used to hash users passwords and compare hashed passwords every time users log in
+const bcrypt = require('bcrypt');
+
 
 // creating movieSchema
 let movieSchema = mongoose.Schema({
@@ -27,6 +30,16 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectID, ref: 'Movie' }]
 });
+
+// // this function is hashing the submitted password
+// userSchema.statics.hashPassword = (password) => {
+//     return bcrypt.hashSync(password, 10);
+// };
+
+// // compares the submitted hashed password with the hashed password stored in my database
+// userSchema.methods.validatePassword = function(password) {
+//     return bcrypt.compareSync(password, this.Password);
+// }
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
