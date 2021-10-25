4c96827b463849264a996f8b8e80a31822698429
florian-grass <89141302+florian-grass@users.noreply.github.com>
2021-10-25 09:16:48 +0200

    Update models.js
    included genre and director schema

--- models.js
+++ models.js
@@ -41,9 +41,28 @@ userSchema.methods.validatePassword = function(password) {
     return bcrypt.compareSync(password, this.Password);
 }
 
+// creating genre schema
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: { type: String, required: true}
+});
+
+// creating directors schema
+let directorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birthdate: { type: Date, required: true},
+    Death: { type: Date},
+    Films: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+});
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 
+// export models
 module.exports.Movie = Movie;
 module.exports.User = User;
-
+module.exports.Genre = Genre;
+module.exports.Director = Director;
